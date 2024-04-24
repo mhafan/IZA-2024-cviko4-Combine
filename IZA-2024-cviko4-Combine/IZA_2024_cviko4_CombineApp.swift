7d7993c39c7e9f85e872128816118019a81c99e2
mhafan <hrubym@me.com>
2024-04-24 10:24:15 +0200

    Initial Commit

---
+++ IZA-2024-cviko4-Combine/IZA_2024_cviko4_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IZA_2024_cviko4_CombineApp.swift
+//  IZA-2024-cviko4-Combine
+//
+//  Created by Martin Hruby on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct IZA_2024_cviko4_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
